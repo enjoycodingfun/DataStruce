14d7ba3741d5129bb2633158a6cb9f1cd7716d31
zz <627882647@qq.com>
2020-03-21 00:32:01 +0800

    leetcode插件初体验

---
+++ datastructure/src/main/java/leetcode/editor/cn/TwoSum.java
@@ -0,0 +1,52 @@
+//给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。 
+//
+// 你可以假设每种输入只会对应一个答案。但是，你不能重复利用这个数组中同样的元素。 
+//
+// 示例: 
+//
+// 给定 nums = [2, 7, 11, 15], target = 9
+//
+//因为 nums[0] + nums[1] = 2 + 7 = 9
+//所以返回 [0, 1]
+// 
+// Related Topics 数组 哈希表
+
+package leetcode.editor.cn;
+public class TwoSum{
+    public static void main(String[] args){
+        Solution solution = new TwoSum().new Solution();
+        int[] nums = {3,2,4};
+        int target = 6;
+        int[] ints = solution.twoSum(nums, target);
+        for (int num : ints) {
+            System.out.print("   " + num);
+        }
+
+
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public int[] twoSum(int[] nums, int target) {
+            if (null == nums || nums.length < 2 ){
+                return null;
+            }
+            int[] arr = new int[2];
+            //int count = 0;
+            for (int i = 0; i < nums.length - 1; i++) {
+                for (int j = i + 1; j < nums.length ; j++) {
+                    if (nums[i] + nums[j] == target){
+                        arr[0] = i;
+                        arr[1] = j;
+                        break;
+                        //count += 2;
+                    }
+                }
+            }
+            return arr;
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
+
+
