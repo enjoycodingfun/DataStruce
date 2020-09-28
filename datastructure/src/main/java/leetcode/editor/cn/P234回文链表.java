f5eefaff1a0e9d24ca8cc9e7bc1d95e3bbf75527
running <627882647@qq.com>
2020-09-28 17:48:53 +0800

    回文链表

--- datastructure/src/main/java/leetcode/editor/cn/P234回文链表.java
+++ datastructure/src/main/java/leetcode/editor/cn/P234回文链表.java
@@ -19,7 +19,6 @@
 
 package leetcode.editor.cn;
 
-import java.util.Stack;
 import leetcode.editor.cn.common.ListNode;
 
 //Java：回文链表
@@ -61,9 +60,64 @@ class Solution {
             }
         }
         return true;*/
-        //2.下面采用先反转链表，再对比是否一致
-
+        //2.下面采用反转链表，再对比是否一致的方法（下面这种方法把原链表已经反转了，所以新老链表永远都一致XXX）
+        //要想使用这种除非构建一个新的链表把原链表保存着
+        /*ListNode newHead = this.reverse(head);
+        //开始遍历对比是否一致
+        while(head != null){
+            if (head.val != newHead.val){
+                return false;
+            }
+            head = head.next;
+            newHead = newHead.next;
+        }
+        return true;*/
+        //3.下面采用先找到中点，然后从中点开始反转后半部分，之后用后半部分与前半部分比较（这个时候前半部分还是原来的样子）
+        //时间复杂度O(N),空间复杂度O(1)
+        //找到中间节点
+        ListNode mid = this.findMid(head);
+        //反转后半部分
+        ListNode newMidHead = this.reverse(mid);
+        //前后两段开始对比
+        while (newMidHead != null){
+            if (newMidHead.val != head.val){
+                return false;
+            }
+            head = head.next;
+            newMidHead = newMidHead.next;
+        }
+        return true;
     }
+
+    private ListNode findMid(ListNode head) {
+        if (head == null || head.next == null){
+            return head;
+        }
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    private ListNode reverse(ListNode head) {
+        //采用迭代法反转链表
+        ListNode pre = null;
+        ListNode cur = head;
+        while (cur != null){
+            //先保存当前节点的下一个节点以防反转后找不到
+            ListNode temp = cur.next;
+            //将当前节点的next指针指向前缀
+            cur.next = pre;
+            //将pre和cur都移动到下一位
+            pre = cur;
+            cur = temp;
+        }
+        return pre;
+    }
+
 }
 //leetcode submit region end(Prohibit modification and deletion)
 
