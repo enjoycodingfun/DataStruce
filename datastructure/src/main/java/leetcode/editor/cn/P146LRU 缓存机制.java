b1673bab7cee060c6bc757c52cc323c781d62281
running <627882647@qq.com>
2020-11-25 23:05:17 +0800

    P146LRU缓存机制（m）

--- datastructure/src/main/java/leetcode/editor/cn/P146LRU 缓存机制.java
+++ datastructure/src/main/java/leetcode/editor/cn/P146LRU 缓存机制.java
@@ -69,89 +69,113 @@ public class P146LRU 缓存机制{
     //leetcode submit region begin(Prohibit modification and deletion)
 class LRUCache {
 
-        class DLinkedNode {
-            int key;
-            int value;
-            DLinkedNode prev;
-            DLinkedNode next;
-            public DLinkedNode() {}
-            public DLinkedNode(int _key, int _value) {key = _key; value = _value;}
+        class ListNode{
+            private int key;
+            private int value;
+            private ListNode pre;
+            private ListNode next;
+
+            public ListNode() {
+            }
+
+            public ListNode(int key, int value) {
+                this.key = key;
+                this.value = value;
+            }
         }
 
-        private Map<Integer, DLinkedNode> cache = new HashMap<Integer, DLinkedNode>();
-        private int size;
+        private Map<Integer, ListNode> cache = new HashMap<>();
         private int capacity;
-        private DLinkedNode head, tail;
+        private ListNode head, tail;
 
         public LRUCache(int capacity) {
-            this.size = 0;
+            cache = new HashMap<>(capacity);
             this.capacity = capacity;
-            // 使用伪头部和伪尾部节点
-            head = new DLinkedNode();
-            tail = new DLinkedNode();
+            //初始化虚拟头结点和尾结点
+            head = new ListNode(-1,-1);
+            tail = new ListNode(-1,-1);
+            //互相指定
             head.next = tail;
-            tail.prev = head;
+            tail.pre = head;
         }
 
         public int get(int key) {
-            DLinkedNode node = cache.get(key);
-            if (node == null) {
+            if (cache.containsKey(key)){
+                ListNode listNode = cache.get(key);
+                int value = listNode.value;
+                //将该节点移动到头部
+                this.moveToFirst(key);
+                return value;
+            }else{
                 return -1;
             }
-            // 如果 key 存在，先通过哈希表定位，再移到头部
-            moveToHead(node);
-            return node.value;
         }
 
+        private void moveToFirst(int key) {
+            //先保存key对应节点
+            ListNode listNode = cache.get(key);
+            //把它前后的节点手牵手
+            listNode.pre.next = listNode.next;
+            listNode.next.pre = listNode.pre;
+            //把当前节点放到虚拟节点头部之后
+            addNodeBehindHead(listNode);
+        }
+
+        private void addNodeBehindHead(ListNode listNode) {
+            //先保存当前真实头结点
+            ListNode oldfirst = head.next;
+            //将虚拟头结点和当前节点手牵手
+            head.next = listNode;
+            listNode.pre = head;
+            //当前节点和老的头结点手牵手
+            listNode.next = oldfirst;
+            oldfirst.pre = listNode;
+        }
+
+        /**
+         * 如果关键字已经存在，则变更其数据值；如果关键字不存在，则插入该组「关键字-值」。当缓存容量达到上
+         * 限时，它应该在写入新数据之前删除最久未使用的数据值，从而为新的数据值留出空间。
+         * @param key
+         * @param value
+         */
         public void put(int key, int value) {
-            DLinkedNode node = cache.get(key);
-            if (node == null) {
-                // 如果 key 不存在，创建一个新的节点
-                DLinkedNode newNode = new DLinkedNode(key, value);
-                // 添加进哈希表
-                cache.put(key, newNode);
-                // 添加至双向链表的头部
-                addToHead(newNode);
-                ++size;
-                if (size > capacity) {
-                    // 如果超出容量，删除双向链表的尾部节点
-                    DLinkedNode tail = removeTail();
-                    // 删除哈希表中对应的项
-                    cache.remove(tail.key);
-                    --size;
-                }
+            //先判断key如果存在直接替换值并把当前节点放到双向链表头部
+            if (cache.containsKey(key)){
+                ListNode listNode = cache.get(key);
+                listNode.value = value;
+                //
+                this.moveToFirst(key);
+                return;
             }
-            else {
-                // 如果 key 存在，先通过哈希表定位，再修改 value，并移到头部
-                node.value = value;
-                moveToHead(node);
+            //如果放入之前先判断容量大小
+            if (cache.size() == capacity){
+                //删除最久未使用的也就是尾结点
+                ListNode oldTail = this.removeLastNode();
+                //别忘了删除cache中的key
+                cache.remove(oldTail.key);
             }
+            //如果容量够则加到头结点上
+            ListNode node = new ListNode(key,value);
+            cache.put(key,node);
+            addNodeBehindHead(node);
         }
 
-        private void addToHead(DLinkedNode node) {
-            node.prev = head;
-            node.next = head.next;
-            head.next.prev = node;
-            head.next = node;
+        /**
+         * 删除双向链表尾部节点
+         * @return
+         */
+        private ListNode removeLastNode() {
+            //找到真实的尾结点
+            ListNode oldTail = tail.pre;
+            oldTail.pre.next = tail;
+            tail.pre = oldTail.pre;
+            //释放引用？
+            oldTail.pre = null;
+            oldTail.next = null;
+            return oldTail;
         }
 
-        private void removeNode(DLinkedNode node) {
-            node.prev.next = node.next;
-            node.next.prev = node.prev;
-        }
-
-        private void moveToHead(DLinkedNode node) {
-            removeNode(node);
-            addToHead(node);
-        }
-
-        private DLinkedNode removeTail() {
-            DLinkedNode res = tail.prev;
-            removeNode(res);
-            return res;
-        }
-
-}
+    }
 
 /**
  * Your LRUCache object will be instantiated and called as such:
