b9ca4a2e7d5d4458fad75780f70bbf902de50c98
running <627882647@qq.com>
2020-11-27 17:15:05 +0800

    P56合并区间（m）

--- datastructure/src/main/java/leetcode/editor/cn/P56合并区间.java
+++ datastructure/src/main/java/leetcode/editor/cn/P56合并区间.java
@@ -44,22 +44,20 @@ public class P56合并区间 {
     class Solution {
 
         public int[][] merge(int[][] intervals) {
-            // 先按照区间起始位置排序
-            Arrays.sort(intervals, (v1, v2) -> v1[0] - v2[0]);
-            // 遍历区间
+            //先按照区间起始值排序
+            Arrays.sort(intervals,(v1,v2)->v1[0]-v2[0]);
             int[][] res = new int[intervals.length][2];
             int idx = -1;
-            for (int[] interval: intervals) {
-                // 如果结果数组是空的，或者当前区间的起始位置 > 结果数组中最后区间的终止位置，
-                // 则不合并，直接将当前区间加入结果数组。
-                if (idx == -1 || interval[0] > res[idx][1]) {
+            for (int[] interval : intervals) {
+                //如果当前结果集为空或者当前遍历到的集合首位数字大于结果集末尾集合的尾部（无交集）就不合并,直接将当前集合添加到尾部
+                if (idx == -1 || interval[0] > res[idx][1]){
                     res[++idx] = interval;
-                } else {
-                    // 反之将当前区间合并至结果数组的最后区间
-                    res[idx][1] = Math.max(res[idx][1], interval[1]);
+                }else{
+                    //反之将当前集合添加到尾部合并起来
+                    res[idx][1] = Math.max(res[idx][1],interval[1]);
                 }
             }
-            return Arrays.copyOf(res, idx + 1);
+            return Arrays.copyOf(res,idx+1);
         }
 
     }
