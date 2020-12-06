c948cfc46da14a5688b2e8555d604167f695bf84
running <627882647@qq.com>
2020-12-06 23:31:44 +0800

    剑指 Offer 07重建二叉树

---
+++ datastructure/src/main/java/leetcode/editor/cn/P剑指 Offer 07重建二叉树.java
@@ -0,0 +1,108 @@
+//输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。 
+//
+// 
+//
+// 例如，给出 
+//
+// 前序遍历 preorder = [3,9,20,15,7]
+//中序遍历 inorder = [9,3,15,20,7] 
+//
+// 返回如下的二叉树： 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 5000 
+//
+// 
+//
+// 注意：本题与主站 105 题重复：https://leetcode-cn.com/problems/construct-binary-tree-from-
+//preorder-and-inorder-traversal/ 
+// Related Topics 树 递归 
+// 👍 264 👎 0
+
+
+package leetcode.editor.cn;
+
+import java.util.HashMap;
+import java.util.Map;
+import leetcode.editor.cn.common.TreeNode;
+
+//Java：重建二叉树
+public class P剑指 Offer 07重建二叉树{
+    public static void main(String[] args) {
+        Solution solution = new P剑指 Offer 07重建二叉树().new Solution();
+        // TO TEST
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    private Map<Integer, Integer> reverses;
+    private int[] preorder;
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        int preLen = preorder.length;
+        int inLen = inorder.length;
+
+        // 可以不做判断，因为题目中给出的数据都是有效的
+        if (preLen != inLen) {
+            return null;
+        }
+
+        this.preorder = preorder;
+
+        // 以空间换时间，否则，找根结点在中序遍历中的位置需要遍历
+        reverses = new HashMap<>(inLen);
+        for (int i = 0; i < inLen; i++) {
+            reverses.put(inorder[i], i);
+        }
+
+        return buildTree(0, preLen - 1, 0, inLen - 1);
+
+    }
+    /**
+     * 根据前序遍历数组的 [preL, preR] 和 中序遍历数组的 [inL, inR] 重新组建二叉树
+     *
+     * @param preL 前序遍历数组的区间左端点
+     * @param preR 前序遍历数组的区间右端点
+     * @param inL  中序遍历数组的区间左端点
+     * @param inR  中序遍历数组的区间右端点
+     * @return 构建的新二叉树的根结点
+     */
+    private TreeNode buildTree(int preL, int preR,
+            int inL, int inR) {
+        if (preL > preR || inL > inR) {
+            return null;
+        }
+        // 构建的新二叉树的根结点一定是前序遍历数组的第 1 个元素
+        int pivot = preorder[preL];
+        TreeNode root = new TreeNode(pivot);
+
+        int pivotIndex = reverses.get(pivot);
+
+        // 这一步得画草稿，计算边界的取值，必要时需要解方程，并不难
+        root.left = buildTree(preL + 1, preL + (pivotIndex - inL), inL, pivotIndex - 1);
+        root.right = buildTree(preL + (pivotIndex - inL) + 1, preR, pivotIndex + 1, inR);
+        return root;
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
