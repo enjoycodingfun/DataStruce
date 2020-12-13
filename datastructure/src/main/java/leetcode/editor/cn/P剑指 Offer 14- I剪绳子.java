1907c1f993202c62938da234469a6c90ba6df0cb
running <627882647@qq.com>
2020-12-13 23:23:09 +0800

    P剑指 Offer 14-I剪绳子-dp

--- datastructure/src/main/java/leetcode/editor/cn/P剑指 Offer 14- I剪绳子.java
+++ datastructure/src/main/java/leetcode/editor/cn/P剑指 Offer 14- I剪绳子.java
@@ -38,8 +38,26 @@ public static void main(String[]args){
 class Solution {
 
     public int cuttingRope(int n) {
-        return method1(n);
+        //return method1(n);
+        return method2(n);
 
+    }
+
+    /**
+     * 方法二：动态规划
+     * https://leetcode-cn.com/problems/jian-sheng-zi-lcof/solution/shu-xue-zhi-shi-he-dong-tai-gui-hua-liang-chong-fa/
+     * @param n
+     * @return
+     */
+    private int method2(int n) {
+        int[] dp = new int[n+1];
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j < i; j++) {
+                dp[i] = Math.max(dp[i],(Math.max(j,dp[j])*Math.max((i-j),dp[i-j])));
+            }
+        }
+        return dp[n];
     }
 
     /**
